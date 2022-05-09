7ee9b832408ea04668bf7d6bf3ba9412da78e788
DeadReBorn <23313785+DeadReBorn@users.noreply.github.com>
2022-05-10 01:21:13 +0200

    Rest of the api done

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const express = require('express');
 const req = require('express/lib/request');
 const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
 require('dotenv/config');
 
 const db = process.env.DB_CONNECTION;
 
+app.use(bodyParser.json());
+
+// Import routes
 const recordsRoute =  require('./routes/records');
 app.use('/records', recordsRoute);
  
+
+// ROUTES 
 app.get('/', (req, res) => {
     res.send('Genesis')
 })
 
 mongoose.connect(db, () =>
-console.log('connected to the DB'));
+console.log('Connected to the DB'));
 
 app.listen(3000);
--- routes/records.js
+++ routes/records.js
@@ -1,14 +1,66 @@
 const express = require('express');
+const { execMap } = require('nodemon/lib/config/defaults');
 const Record = require ('../models/Record');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('See you on a dark night')
+
+// Get all the records
+router.get('/getAllRecords', async (req, res) => {
+    try {
+        const allRecords = await Record.find();
+        res.json(allRecords);
+    } catch (err) {
+        res.json({message: err})
+    }
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-}); 
+// Add a record
+router.post('/addRecord', async (req, res) => {
+    const record = new Record({
+        title: req.body.title,
+        artist: req.body.artist,
+        year: req.body.year,
+    });
+    try {
+        const savedRecord = await record.save();
+        res.json(savedRecord);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
+
+// Get one record
+router.get('/getRecord/:recordId', async (req, res) => {
+    try {
+        const foundRecord = await Record.findById(req.params.recordId);
+        res.json(foundRecord);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
+
+// Delete one record
+router.delete('/deleteRecord/:recordId', async (req, res) => {
+    try {
+        const deletedRecord = await Record.remove({_id: req.params.recordId});
+        res.json(deletedRecord);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
+
+// Update one record
+router.patch('/updateRecord/:recordId', async (req, res) => {
+    try {
+        const updatedRecord = await Record.updateOne(
+            {_id: req.params.recordId},
+            {$set: { title: req.body.title, artist: req.body.artist, year: req.body.year }
+        });
+        res.json(updatedRecord);
+    } catch (err) {
+        res.json({message: err})
+    }
+});
 
 module.exports = router; 
